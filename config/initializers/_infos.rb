7413a1cf2105e291a85911a6a8954ba1d4268c4c
Ruijian Cao <cao7113@hotmail.com>
2011-07-09 10:49:41 +0800

    modify some link and config

--- config/initializers/_infos.rb
+++ config/initializers/_infos.rb
@@ -1,1 +1,4 @@
-PROVERB="把发现的精彩分享给身边的人！～～～"
+PROVERB=">把发现的精彩分享给身边的人！～～～"
+CAORY="Caory"
+CAORY_HEAD_ICON_URL="http://tp4.sinaimg.cn/1809866795/50/5599211708/1"
+CAORY_T_URL="http://weibo.com/cao7113/"
